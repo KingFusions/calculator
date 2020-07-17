361887e4cd9289fad5d0cb9c145b68d27e65ee04
Stephen Bautista <spaulbautista@gmail.com>
2020-07-17 20:32:35 +0800

    Updates

---
+++ precache-manifest.938018b1518c5965856d7e9a5e7f3b61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fa0f329486daa6fe020faeb87f1781b",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "4d176f30c56eda0c93c7",
+    "url": "/calculator/static/css/main.c275069e.chunk.css"
+  },
+  {
+    "revision": "ecada51e1a3334c42b99",
+    "url": "/calculator/static/js/2.e6f076d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.e6f076d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d176f30c56eda0c93c7",
+    "url": "/calculator/static/js/main.ad40d292.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
